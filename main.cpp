850e09d39dc4df7eaa6fc8ac5b7cf18f038c512d
andrii-francuz <andrii.fryntsko@gmail.com>
2020-12-10 07:19:06 +0100

    It's just a start

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    int random_number;
+    
+    std::cout << "Enter random number: ";
+    std::cin >> random_number;
+    std::cout << "Ok" << std::endl;
+    
+    return 0;
+}
